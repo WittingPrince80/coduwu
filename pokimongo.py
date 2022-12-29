afa22e7578ac420042025ae153bb3dec2ec17e8d
WittingPrince80 <87693035+WittingPrince80@users.noreply.github.com>
2022-12-29 15:00:40 +1100

    Add files via upload

---
+++ pokimongo.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import random
+
+root=Tk()
+root.title("Endless Pokimon game")
+root.geometry("600x400")
+root.configure(background="chocolate1")
+
+img=ImageTk.PhotoImage(Image.open ("button.jpg"))
+
+player1 = Label(root,text = "Player 1", bg = "royal blue", fg = "white")
+player1.place(relx = 0.1, rely = 0.3 , anchor = CENTER)
+
+player2 = Label(root,text = "Player 2", bg = "royal blue", fg = "white")
+player2.place(relx = 0.9, rely = 0.3 , anchor = CENTER)
+
+player1_score_label = Label(root , bg = "red", fg = "white")
+player1_score_label.place(relx = 0.1, rely = 0.4 , anchor = CENTER)
+
+player2_score_label = Label(root , bg = "red", fg = "white")
+player2_score_label.place(relx = 0.9, rely = 0.4 , anchor = CENTER)
+
+random_dice_label = Label(root,bg = "white", fg = "black")
+random_dice_label.place(relx = 0.5, rely = 0.5 , anchor = CENTER)
+
+
+player1_score = 0
+def player1():
+    global player1_score
+    pokemon_list = ["pikachu.jpg","bulbasour.jpg","abra.jpg","charmander.jpg","lyvasour.jpg"]
+    pokiHP = ["200","60","30","50","100"]
+    random_no = random.randint(0,4)
+    random_dice_label["text"] = "Player 1 Dice Random Number : " + str(random_no)
+    player1_score = player1_score + pokiHP[random_no]
+    player1_score_label["text"] = str( player1_score)
+    
+player_1_btn = Button(root, image = img, command = player1)
+player_1_btn.place(relx = 0.1, rely = 0.6 , anchor = CENTER)
+
+player2_score = 0
+def player2():
+    global player2_score
+    pokemon_list = ["pikachu.jpg","bulbasour.jpg","abra.jpg","charmander.jpg","lyvasour.jpg"]
+    pokiHP = ["200","60","30","50","100"]
+    random_no2 = random.randint(0,4)
+    random_dice_label["text"] = "Player 2 Dice Random Number : " + str(random_no2)
+    player2_score = player2_score + pokiHP[random_no2]
+    player2_score_label["text"] = str( player2_score)
+    
+player_2_btn = Button(root, image = img, command = player2)
+player_2_btn.place(relx = 0.9, rely = 0.6 , anchor = CENTER)
+root.mainloop()
